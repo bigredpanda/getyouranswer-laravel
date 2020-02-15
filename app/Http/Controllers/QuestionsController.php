16a2665a11ffec3cefb380f00d0d2862dda3d032
Pavel Spichak <pavel.spichak@greenice.net>
2020-02-15 07:51:08 +0200

    implement gates for update/delete questions

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AskQuestionRequest;
 use App\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class QuestionsController extends Controller
 {
@@ -66,6 +67,10 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        if (Gate::denies('update-question', $question)) {
+            abort(403, 'Access denied');
+        }
+
         return view('questions.edit', compact('question'));
     }
 
@@ -78,6 +83,10 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if (Gate::denies('update-question', $question)) {
+            abort(403, 'Access denied');
+        }
+
         $question->update($request->only('title', 'body'));
 
         return redirect()->route('questions.index')->with('success', 'Your question has been edited.');
@@ -92,6 +101,10 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        if (Gate::denies('delete-question', $question)) {
+            abort(403, 'Access denied');
+        }
+
         $question->delete();
 
         return redirect()->route('questions.index')->with('success', 'Your question has been deleted');
--- resources/views/questions/_action_buttons.blade.php
+++ resources/views/questions/_action_buttons.blade.php
@@ -1,16 +1,20 @@
 <div class="ml-auto actions">
-    <a href="{{ route('questions.edit', $question->id) }}"
-       class="btn btn-outline-info btn-sm">
-        Edit
-    </a>
+    @if(auth()->user()->can('update-question', $question))
+        <a href="{{ route('questions.edit', $question->id) }}"
+           class="btn btn-outline-info btn-sm">
+            Edit
+        </a>
+    @endif
 
-    <form action="{{ route('questions.destroy', $question->id) }}"
-          class="form-delete" method="POST">
-        @csrf {{method_field('DELETE')}}
+    @if(auth()->user()->can('delete-question', $question))
+        <form action="{{ route('questions.destroy', $question->id) }}"
+              class="form-delete" method="POST">
+            @csrf {{method_field('DELETE')}}
 
-        <button class="btn btn-outline-danger btn-sm"
-                onclick="return confirm('Are you sure>'); " type="submit">
-            Delete
-        </button>
-    </form>
+            <button class="btn btn-outline-danger btn-sm"
+                    onclick="return confirm('Are you sure>'); " type="submit">
+                Delete
+            </button>
+        </form>
+    @endif
 </div>
