08765af1487a29ef3810d83894cb49ba6733881f
Pavel Spichak <pavel.spichak@greenice.net>
2020-02-19 16:53:42 +0200

    added fontawesome, question&answers vote controls markup

---
+++ resources/views/questions/_vote_controls.blade.php
@@ -0,0 +1,7 @@
+<a title="{{$voteUpTitle}}" class="vote-up">
+    <i class="fas fa-caret-up fa-3x"></i>
+</a>
+<span class="votes-count">10</span>
+<a title="{{$voteDownTitle}}" class="vote-down off">
+    <i class="fas fa-caret-down fa-3x"></i>
+</a>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,35 +5,47 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card question-block">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h2>{{ $question->title }}</h2>
-
-                            @include('questions._action_buttons', compact('question'))
-                        </div>
-                    </div>
-
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h2>{{ $question->title }}</h2>
 
+                                @include('questions._action_buttons', compact('question'))
+                            </div>
+                        </div>
+
+                        <hr/>
 
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls mr-5">
+                                @include('questions._vote_controls', [
+                                    'voteUpTitle'   => 'This question is useful',
+                                    'voteDownTitle' => 'This question is not useful',
+                                ])
+
+                                <a title="Click to mark as favorite (Click again to undo)"
+                                   class="favorite favorited mt-2">
+                                    <i class="fas fa-star fa-2x "></i>
+                                    <span class="favorites-count">4</span>
+                                </a>
+                            </div>
+
                             <div class="media-body">
-                                {!! $question->body_html !!}
+                                <div class="media">
+                                    <div class="media-body">
+                                        {!! $question->body_html !!}
 
-                                <div class="float-right">
-                                    <span class="text-muted small">Asked {{$question->created_date}}</span>
-                                    <div class="media mt-1" style="min-width: 150px">
-                                        <a href="{{$question->author->url}}" class="pr-2">
-                                            <img src="{{$question->author->avatar}}">
-                                        </a>
+                                        <div class="float-right">
+                                            <span class="text-muted small">Asked {{$question->created_date}}</span>
+                                            <div class="media mt-1" style="min-width: 150px">
+                                                <a href="{{$question->author->url}}" class="pr-2">
+                                                    <img src="{{$question->author->avatar}}">
+                                                </a>
 
-                                        <div class="media-body mt-2 small">
-                                            <a href="{{$question->author->url}}">{{$question->author->name}}</a>
+                                                <div class="media-body mt-2 small">
+                                                    <a href="{{$question->author->url}}">{{$question->author->name}}</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -60,6 +72,18 @@
 
                             @foreach($question->answers as $answer)
                                 <div class="media">
+                                    <div class="d-flex flex-column vote-controls mr-5">
+                                        @include('questions._vote_controls', [
+                                            'voteUpTitle'   => 'This answer is useful',
+                                            'voteDownTitle' => 'This answer is not useful',
+                                        ])
+
+                                        <a title="Click to mark as best answer (Click again to undo)"
+                                           class="vote-accept mt-2">
+                                            <i class="fas fa-check fa-2x "></i>
+                                        </a>
+                                    </div>
+
                                     <div class="media-body">
                                         {!! $answer->body !!}
 
